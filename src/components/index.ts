bd04e2838cd4b1b6d7a91fee06f3500524206c52
Barry Anil <anilkumar.b@intelliconnectq.com>
2024-01-24 19:27:36 +0530

    Admin_Kanban_Dashboard

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,23 @@
-export { Header } from "./header";
+import { UpcomingEvents } from "./home";
+import { DealsChart } from "./home";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import DashboardTotalCoundCard from './home/total-count-card' 
+import DashboardLatestActivities from './home/latest-activities'
+import CustomAvatar from './custom-avatar'
+
+export { 
+    UpcomingEvents, 
+    DealsChart, 
+    UpcomingEventsSkeleton, 
+    AccordionHeaderSkeleton, 
+    KanbanColumnSkeleton, 
+    ProjectCardSkeleton, 
+    LatestActivitiesSkeleton, 
+    DashboardTotalCoundCard, 
+    DashboardLatestActivities,
+    CustomAvatar
+ }
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,13 @@
+import { ThemedLayoutV2, ThemedTitleV2 } from '@refinedev/antd'
+import Header from './header'
+
+const Layout = ({children} : React.PropsWithChildren) => {
+  return (
+    <ThemedLayoutV2
+        Header={Header}
+        Title={(titleprops) => <ThemedTitleV2 {...titleprops} text="Refine" />}
+    >{children}</ThemedLayoutV2>
+  )
+}
+
+export default Layout
